e1f2058cf520db6bb3de0be0a04382de6848f3da
Password-Is-Taco <duke@markforged.com>
2016-02-09 15:29:33 -0500

    Create zendesk.js
    Copying the Zendesk JavaScript

---
+++ zendesk.js
@@ -0,0 +1,44 @@
+/*
+ * jQuery v1.9.1 included
+ */
+
+$(document).ready(function() {
+
+  // social share popups
+  $(".share a").click(function(e) {
+    e.preventDefault();
+    window.open(this.href, "", "height = 500, width = 500");
+  });
+
+  // toggle the share dropdown in communities
+  $(".share-label").on("click", function(e) {
+    e.stopPropagation();
+    var isSelected = this.getAttribute("aria-selected") == "true";
+    this.setAttribute("aria-selected", !isSelected);
+    $(".share-label").not(this).attr("aria-selected", "false");
+  });
+
+  $(document).on("click", function() {
+    $(".share-label").attr("aria-selected", "false");
+  });
+
+  // show form controls when the textarea receives focus
+  $(".answer-body textarea").one("focus", function() {
+    $(".answer-form-controls").show();
+  });
+
+  $(".comment-container textarea").one("focus", function() {
+    $(".comment-form-controls").show();
+  });
+  
+  $("ul.article-list li a").each(function(i, e) {
+    var e = $(e);
+    var link = e.attr('href');
+    var path = window.location.pathname;
+    if (path.indexOf(link) > -1) {
+      e.parent().addClass('active');
+      // $('.sidebar-container').scrollTop(e.offset().top - 134);
+    }
+  });
+
+});
